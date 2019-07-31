7e33f8aded49e5fd4415ce14e545a481f60712b4
Jeff Hansen <dscheffy@gmail.com>
2019-07-31 17:45:46 +0200

    Wire up unit testing frameworks
    There are so many, I don't really know which to choose from or why. I'm
    just following some blogs for now. I've worked with a few of them
    already and mocha and chai have come up along the way as well as jasmine
    and karma. I'll start with these for now and possibly switch over later
    if it makes sense.

---
+++ src/tree.test.ts
@@ -0,0 +1,11 @@
+import { IndexedTree } from "./tree";
+
+import * as mocha from "mocha";
+import * as chai from "chai";
+
+const expect = chai.expect;
+describe('Some tree tests', () => {
+    it('should work', () => {
+        expect(5+4).to.equal(9);
+    })
+})
