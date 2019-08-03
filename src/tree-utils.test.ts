f64e3e5d19580fb3993ddb0880f24afff80860b2
Jeff Hansen <dscheffy@gmail.com>
2019-08-03 15:38:08 +0200

    Create more tree util functions

--- src/tree-utils.test.ts
+++ src/tree-utils.test.ts
@@ -1,4 +1,4 @@
-import { expandTree, validateTree } from "./tree-utils";
+import { branchNodes, expandTree, maxDepth, relativeAt, validateTree } from "./tree-utils";
 import { TreeExpansion, TreeIndex } from "./types";
 
 import * as chai from "chai";
@@ -6,10 +6,12 @@ import * as mocha from "mocha";
 
 
 const expect = chai.expect;
-const goodTree: TreeIndex = {3: "003", 4: "004", 5: "005"};
+const goodTree: TreeIndex = {3: "003", 4: "008", 5: "010"};
+const branch2: TreeIndex = {4: "008", 5: "010"};
+const branch3: TreeIndex = {3: "003"};
 const badTree: TreeIndex = {3: "003", 4: "004"};
 const expansion: TreeExpansion = { 4: {8: "008", 9: "009"}, 5: {10: "010", 11: "011"}};
-const expandedTree: TreeExpansion = {3: "003", 8: "008", 9: "009", 10: "010", 11: "011"};
+const expandedTree: TreeIndex = {3: "003", 8: "008", 9: "009", 10: "010", 11: "011"};
 
 describe('Some validation tests', () => {
     it('should validate a good tree', () => {
@@ -32,3 +34,29 @@ describe('Some tree expansion tests', () => {
 })
 
 
+describe('Some relative branch tests', () => {
+    it('should be able to find relative nodes', () => {
+        expect(relativeAt(expandedTree,2,0)).equals(8);
+        expect(relativeAt(expandedTree,2,1)).equals(11);
+        expect(relativeAt(expandedTree,2,2)).equals(8);
+        expect(relativeAt(expandedTree,2,3)).equals(11);
+        expect(relativeAt(expandedTree,2,8)).equals(8);
+        expect(relativeAt(expandedTree,2,9)).equals(8);
+    })
+})
+
+describe('Some max depth tests', () => {
+    it('should be able to find max depth', () => {
+        expect(maxDepth(expandedTree,2)).equals(2);
+        expect(maxDepth(expandedTree,3)).equals(0);
+        expect(maxDepth(expandedTree,1)).equals(3);
+        expect(maxDepth(expandedTree,5)).equals(1);
+    })
+})
+
+describe('Some branch node tests', () => {
+    it('should be able to filter nodes of a branch', () => {
+        expect(branchNodes(goodTree,2)).deep.equals(branch2);
+        expect(branchNodes(goodTree,3)).deep.equals(branch3);
+    })
+})
--- src/tree-utils.ts
+++ src/tree-utils.ts
@@ -1,7 +1,7 @@
 import { TreeExpansion, TreeIndex } from "./types";
 
-const maxDepth = 30;
-const maxIndex = 1 << (maxDepth + 1) -1;
+const MAX_DEPTH = 30;
+const MAX_INDEX = 1 << (MAX_DEPTH + 1) -1;
 
 /**
  * Checks if a branch is valid -- as in it has full coverage and no missing t
@@ -20,7 +20,7 @@ const validateBranch = (t: TreeIndex, b: number): boolean => {
     return true;
   }
   // fail if we've exceeded the max depth
-  if(b>maxIndex) {
+  if(b>MAX_INDEX) {
     return false;
   }
   // check both of the sub branches recursively
@@ -52,4 +52,36 @@ export const expandTree = (t: TreeIndex, tx: TreeExpansion): TreeIndex => {
     }
   }
   return expanded;
+}
+
+export const maxDepth = (t: TreeIndex, b: number): number => {
+  if (t[`${b}`]) {
+    return 0;
+  }
+  return(Math.max(maxDepth(t,b*2)+1, maxDepth(t,b*2+1)+1));
+}
+
+export const relativeAt = (t: TreeIndex, b: number, rb: number): number => {
+  // we can't mod something with 0, so we have to use a minimum depth of 1
+  // this will mean treating rb=0 the same as 2 and rb=1 the same as 3.
+  const depth = Math.max(Math.floor(Math.log2(rb)), 1);
+  let n = b;
+  for(let i = 1; n<=MAX_INDEX && i<MAX_DEPTH; i++) {
+    if (t[`${n}`]) { return(n) }
+    const shift = i % depth; 
+    n *= 2;
+    n = n + ((rb>>shift) & 1);
+  }
+  return null;
+}
+
+export const branchNodes = (t: TreeIndex, b: number): TreeIndex => {
+  if (t[`${b}`]) {
+    const r = {};
+    r[`${b}`]=t[`${b}`];
+    return r;
+  }
+  const lb = branchNodes(t,b*2);
+  const rb = branchNodes(t,b*2+1);
+  return{...lb, ...rb};
 }
