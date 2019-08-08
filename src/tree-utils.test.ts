5907e096561c82713b4ddaeb4e2e30ec3f01dcfa
Jeff Hansen <dscheffy@gmail.com>
2019-08-08 12:37:07 +0200

    Add some more tree splitting tests

--- src/tree-utils.test.ts
+++ src/tree-utils.test.ts
@@ -89,7 +89,6 @@ describe('Some tree splitting tests', () => {
         expect(validateTree(separated.core)).is.true;
         expect(separated.core["11"]).equals("022");
         expect(separated.core["12"]).equals("096");
-//        console.log(JSON.stringify(separated))
         let rehydrated = separated.core;
         delete(separated.core);
         Object.keys(separated).forEach(k => {
@@ -98,5 +97,50 @@ describe('Some tree splitting tests', () => {
         expect(rehydrated).deep.equals(bigTree);
         expect(Object.keys(separated).length).equals(3) // baskets 1, 5 and 6
     })
+    it('should split a perfectly balanced tree', () => {
+        const depth = 7;
+        const size = 1<<depth;
+        expect(size).to.equal(128);
+        const tree = {};
+        Array.from(Array(size).keys()).map(x=>x+size).forEach(x=>{tree[(x)]=x.toString(16)});
+        expect(validateTree(tree)).is.true
+        const separated = splitTree(tree, 4, 2);
+        const core = {...separated.core};
+        expect(Object.keys(core).length).to.equal(16)
+        const expanded = expandTree(core,separated[1]);
+        expect(Object.keys(expanded).length).to.equal(64)
+        expect(separated[2]).is.not.ok;
+        expect(separated[3]).is.not.ok;
+        expect(separated[4]).is.ok;
+        let rehydrated = separated.core;
+        delete(separated.core);
+        Object.keys(separated).forEach(k => {
+            rehydrated = expandTree(rehydrated, separated[k])
+        })
+        expect(rehydrated).deep.equals(tree);
+    })
+    it('should split adams apple correctly', () => {
+        // this was the actual first tree that we were using with adams apple
+        // (or at least the keys -- I've replaced the values with hex key values)
+        const adam = [33,34,35,37,38,39,41,42,43,45,46,47,49,50,51,53,54,55,57,58,59,61,62,63,64,65,72,73,80,81,88,89,96,97,104,105,112,113,120,121]
+        const tree = {};
+        adam.forEach(x=>{tree[(x)]=x.toString(16)});
+        expect(validateTree(tree)).is.true
+        const separated = splitTree(tree, 2, 2);
+        const core = {...separated.core};
+        expect(Object.keys(core).length).to.equal(4)
+        const expanded = expandTree(core,separated[1]);
+        expect(Object.keys(expanded).length).to.equal(16)
+        expect(separated[2]).is.not.ok;
+        expect(separated[3]).is.not.ok;
+        expect(separated[4]).is.ok;
+        let rehydrated = separated.core;
+        delete(separated.core);
+        Object.keys(separated).forEach(k => {
+            rehydrated = expandTree(rehydrated, separated[k])
+        })
+        expect(rehydrated).deep.equals(tree);
+    })
+
 })
 
